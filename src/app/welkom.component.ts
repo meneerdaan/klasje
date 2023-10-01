d5e8ca53e1dac962c83b7b9b5f2f171e088d044c
Daan van den Akker <daan.vandenakker@duo.nl>
2023-10-01 09:53:07 +0200

    meerdere personen in tabel

--- src/app/welkom.component.ts
+++ src/app/welkom.component.ts
@@ -3,13 +3,21 @@ import {Persoon} from "./persoon";
 
 @Component({
   selector: 'klasje-welkom',
-  templateUrl: './welkom.component.html'
+  templateUrl: './welkom.component.html',
+  styleUrls: ['./welkom.css']
 })
 export class WelkomComponent {
 
-  persoon: Persoon = {
-    id: 1,
-    naam: 'Bas',
-    hobby: 'Op rotsen klimmen'
-  }
+  persoon: Persoon[] = [
+    {
+      id: 1,
+      naam: 'Bas',
+      hobby: 'Op rotsen klimmen'
+    },
+    {
+      id: 2,
+      naam: 'Daan',
+      hobby: 'Lekker op de grond blijven'
+    }
+  ]
 }
