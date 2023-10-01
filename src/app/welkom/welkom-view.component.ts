f9433f59fe47d609b15c1007ca34debe2d8a3a45
Daan van den Akker <daan.vandenakker@duo.nl>
2023-10-01 10:09:02 +0200

    tabel naar view

--- src/app/welkom/welkom-view.component.ts
+++ src/app/welkom/welkom-view.component.ts
@@ -1,9 +1,12 @@
-import {Component} from "@angular/core";
+import {Component, Input} from "@angular/core";
+import {Persoon} from "../persoon";
 
 @Component({
   selector: 'klasje-welkom-view',
   templateUrl: 'welkom-view.component.html'
 })
 export class WelkomViewComponent {
+
+  @Input() personen!: Persoon[];
 
 }
