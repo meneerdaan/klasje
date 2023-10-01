d9113e81edfe8484fa9f2c71945ad058522e7d42
Daan van den Akker <daan.vandenakker@duo.nl>
2023-10-01 09:29:58 +0200

    welkom in html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {WelkomComponent} from "./welkom.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelkomComponent
   ],
   imports: [
     BrowserModule,
